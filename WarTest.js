6354cff9a3084ccb24376569b01987536090a964
camkiel2 <137121017+camkiel2@users.noreply.github.com>
2023-07-22 16:52:44 -0400

    Add files via upload

---
+++ WarTest.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const { Deck } = require('./Week6Project.js'); // Replace with the path to your deck file
+
+describe('Deck', () => {
+  describe('#shuffleDeck()', () => {
+    it('should return a shuffled deck of cards', () => {
+      const deck = new Deck();
+      deck.createDeck();
+
+      
+      const originalDeckOrder = deck.cards.map(card => `${card.suit} ${card.name}`);
+
+      
+      const shuffledDeck = deck.shuffleDeck();
+
+      
+      expect(shuffledDeck).to.have.lengthOf(deck.cards.length);
+
+      r
+      const shuffledDeckOrder = shuffledDeck.map(card => `${card.suit} ${card.name}`);
+      expect(shuffledDeckOrder.sort()).to.deep.equal(originalDeckOrder.sort());
+      expect(shuffledDeckOrder).to.not.deep.equal(originalDeckOrder);
+    });
+  });
+});
+
